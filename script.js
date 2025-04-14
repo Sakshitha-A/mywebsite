a332677dff2fb743e63f8b94a888c9e5bae21db1
Sakshitha-A <142290430+Sakshitha-A@users.noreply.github.com>
2025-04-14 10:12:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const aboutMeDiv = document.querySelector('.About-Me');
-let scrolledPastInitial = false;
-
-window.addEventListener('scroll', () => {
-  console.log('Scroll event fired. window.scrollY:', window.scrollY);
-  if (!scrolledPastInitial && window.scrollY > 0) {
-    console.log('yre - Condition met!');
-    scrolledPastInitial = true;
-    document.body.style.overflowY = 'auto'; // Enable vertical scrolling
-    aboutMeDiv.classList.toggle('visible'); // Use toggleClass for About Me
-  }
+$(document).ready(function() {
+  $(".hamburger-menu").click(function() {
+    $(".nav-links").toggleClass("mobile-menu");
+  });
 });
